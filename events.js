5b20445376f39435e71f86a17d8d1d5932ead668
eliudkaruga <eliudkaruga97@gmail.com>
2023-08-15 16:31:26 +0300

    end of eventss

--- events.js
+++ events.js
@@ -226,7 +226,7 @@ logo.addEventListener('click', function(e) {
 
 //to prevent default behaviour we use preventdefault method on e. 
 //if you have a link on your html the default behaviour on clicking the link is that it takes you to the linked page
-//you can prevebnt this using the .preventdeault() method;
+//you can prevent this using the .preventdeault() method;
 
 document.querySelector('a').addEventListener('click', function(e){
     e.preventDefault();
@@ -277,9 +277,168 @@ const specific = (e) => {
         console.log("You're holding down " + e.key)
     }
 
-    //to see if shift key is presseddown
+    //to see if shift key is presseddown returns true/false
     console.log( "Shift: " + e.shiftKey);
     console.log( "Control: " + e.ctrlKey);
     console.log( "Alt: " + e.altKey);
+
+    if (e.shiftKey && e.key === "K") {
+        console.log('You hit shift + K')
+    }
 };
 document.querySelector('.input').addEventListener('keydown', specific)
+
+const input = document.getElementById('item-input');
+const priorityInput = document.getElementById('priority-input');
+const checkBox = document.getElementById('checkbox');
+const headingOne = document.getElementById('heading-one');
+
+//the ideal event listener for inputs fields/forms is 'input'. you can also use keydown but input is more ideal
+//this because an event like keydown may not work on input fields such as a select list
+
+const onInput = (e) => {
+    console.log('input');
+    console.log(e.target.value); 
+    //headingOne.textContent = e.target.value;
+}
+
+input.addEventListener('input', onInput);
+
+//priorityInput.addEventListener('input', onInput)
+//you can also use change for select option field forms
+priorityInput.addEventListener('change', onInput);
+
+//for checkbox forms, we use the checked on the event object to determine whether it's true or false
+const onChecked = (e) => {
+    console.log(e.target.checked)
+    const isChecked = e.target.checked;
+    headingOne.textContent = isChecked ? 'Checked' : 'notChecked';
+};
+
+checkBox.addEventListener('input', onChecked);
+
+//focus and blur event listeners for inputs 
+
+const onFocus = (e) => {
+    console.log('input is focused');
+    input.style.outlineStyle = 'solid';
+    input.style.outlineWidth = '4px';
+    input.style.outlineColor = 'green';
+};
+
+input.addEventListener('focus', onFocus)
+
+const onBlur = (e) => {
+    console.log('its out of focus');
+    input.style.outlineStyle = 'none';
+} 
+
+input.addEventListener('blur', onBlur);
+
+//submit event listener 
+
+function onSubmit (e) {
+    e.preventDefault()
+    console.log('submit')
+
+   const itemsInput = document.getElementById('item-input').value;
+   const prior = document.getElementById('priority-input').value;
+
+   if(itemsInput === '' || prior === '0') {
+    alert("please fill all fields");
+    return;
+   }
+
+   console.log(itemsInput, prior);
+}
+
+
+const form = document.getElementById('myForm')
+form.addEventListener('submit' , onSubmit);
+
+//form data - creates a new instance of an empty formdata object that can be used to store key/values pairs
+// the form passed in the brackets is the const form - a global variable
+const onSubmit2 = (e) => {
+    e.preventDefault();
+    
+//this creates a Formdata from the html form.FormData is an object {key:value} pay attention to the uppercase 
+  const formData = new FormData (form);
+  console.log(formData);
+
+    //an alternative to using .value to get the user input 
+    // cont variable = form.get('name string')
+    const lulu = formData.get('itemz');
+    const selection = formData.get('priority');
+    console.log(lulu, selection);
+
+    //you can also use the .entries method which returns an iterator. loop the iterator to get the user input
+    const entries = formData.entries()
+    console.log(entries);
+
+    for (let entry of entries) {
+        console.log(entry)
+    }
+
+    
+};
+
+form.addEventListener('submit' ,onSubmit2);
+
+
+//event bubbling - happens when a targeted element receives an event, and that event bubbles up/transimmitted/ propagated 
+//to its parent and ancestor elements in the DOM tree until it gets to the root element.
+//picture an arrow moving from the targeted element upwards to the parent
+const inputB = document.getElementById('item-input');
+inputB.addEventListener('click', () => {
+    alert("parent div was clicked")
+})
+
+
+const pay = document.getElementById('payments');
+pay.addEventListener('click', () => {
+    alert('you are proceeding to payments')
+});
+
+//the event is fired on the checkout button, then propageted to upwards to the parent. 
+//in the process of propagation any other parent that has the same event type is also fired/activated
+//in this case the checkout button is event is activated and then its propagated upwards, 
+//where it also activates the 'go to cart' which also has the same event type of click 
+
+//better demonstration of event bubbling 
+
+const parentBub = document.getElementById('parent');
+parentBub.addEventListener('click', function() {
+    alert(`this is a parent`);
+})
+
+//you can stop this event transmission/propagation to other elements using e.stopPropagation
+const childEvn = document.getElementById('child');
+childEvn.addEventListener('click', (e) => {
+    alert (`this is a child`);
+    e.stopPropagation()
+})
+
+
+//event delegation 
+//-  attaching one listener to multiple children at once instead of attaching them individually
+//1- the basic way - attaching to each child individually 
+
+const markt = document.querySelectorAll('.goods');
+
+//markt.addEventListener('click', (e) => console.log(e.target))
+/*markt.forEach((item) => {
+    item.addEventListener('click', (e)=> e.target.remove());
+})*/
+
+//2 delegation - attaching a single event listener to a parent element and then target which child gets affected by the event 
+
+ const carier = document.querySelector('.groceries');
+ carier.addEventListener('click', (e) => { 
+    if(e.target.className === "goods") {
+        e.target.remove()
+    }
+    
+ });
+
+//window events 
+
